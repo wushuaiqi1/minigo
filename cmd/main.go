c16c45664c174c3950b5226db78c603da590169b
wushuaiqi1 <1262458648@qq.com>
2026-04-17 15:10:19 +0800

    优雅关闭服务器

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"minigo/config"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/kataras/iris/v12"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
@@ -18,12 +24,39 @@ func main() {
 		})
 	})
 
-	err := app.Listen(config.GetConfigInstance().Server.Port)
-	if err != nil {
-		return
-	}
+	// 在 goroutine 中启动服务器
+	go func() {
+		logrus.WithFields(logrus.Fields{"port": config.GetConfigInstance().Server.Port}).Info("Server is starting")
+		if err := app.Listen(config.GetConfigInstance().Server.Port); err != nil {
+			logrus.WithError(err).Error("Server error")
+		}
+	}()
+
+	gracefulShutdown(app)
 }
 
 func setRoute() {
 
+}
+
+// 优雅关闭服务器
+func gracefulShutdown(app *iris.Application) {
+	// 优雅关闭服务器	// 创建通道用于监听系统信号
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	// 等待退出信号
+	sig := <-quit
+	logrus.WithFields(logrus.Fields{"signal": sig}).Info("Server is shutting down")
+
+	// 创建超时上下文，最多等待 30 秒完成正在处理的请求
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	// 优雅关闭服务器
+	if err := app.Shutdown(ctx); err != nil {
+		logrus.WithError(err).Error("Server shutdown error")
+	}
+
+	logrus.Info("Server has been shutdown")
 }
